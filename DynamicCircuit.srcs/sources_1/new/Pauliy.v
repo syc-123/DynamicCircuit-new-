4ea2a66f918df60ff2ab58ad26a8311d391bdd75
Yicheng Song <yicheng.song@mail.mcgill.ca>
2023-09-27 10:15:45 -0400

    Initial commit

---
+++ DynamicCircuit.srcs/sources_1/new/Pauliy.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/09/27 10:14:05
+// Design Name: 
+// Module Name: Pauliy
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Pauliy(
+
+    );
+endmodule
