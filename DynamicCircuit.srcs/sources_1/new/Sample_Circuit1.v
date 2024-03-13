410581c9e26338be3c543b2b5ddd88e18f6af5fd
Yicheng Song <yicheng.song@mail.mcgill.ca>
2024-03-13 10:01:58 -0400

    sample circuit 1 added

---
+++ DynamicCircuit.srcs/sources_1/new/Sample_Circuit1.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/03/13 09:49:10
+// Design Name: 
+// Module Name: Sample_Circuit1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Sample_Circuit1(
+    input [31: 0] i_q0,
+    input [31: 0] i_q1,
+    output [31: 0] o_q0,
+    output [31: 0] o_q1
+    );
+endmodule
